e90be6f7e2607b102fc3b56bfca509140cb75f93
Gurpreet Singh Dhami <gdhami@gmail.com>
2019-04-26 15:23:41 -0400

    Vector practice

--- vector.cpp
+++ vector.cpp
@@ -1,70 +1,82 @@
-/*  Objectives : 
-- Create a vector of ints of size 10. Sort it . Print it. Print 5th the element
-- Create a vector of string. Same as above.
-- Traversing vector using iterator
-*/
+/*  Objectives :
 
+- Practice
+*/
 
 #include <iostream>
 #include <vector>
-#include <iterator>
-#include <string>
+
 using namespace std;
-void forintegers();
-void print_vector(vector <int> vec);
-int main()
-{
+class vector_practice {
+private:
+  vector<int> vec;
 
-forintegers();
-}
+public:
+  void vec_add(void);
+  void vec_list(void);
+  void vec_insert();
+  // void vec_assign();
+  // void vec_push_back();
+  // void vec_emplace_back();
+  // void vec_erase();
+  // void vec_clear();
+};
 
+void vector_practice::vec_add(void) {
+  int num;
+  while (1) {
+    cout << "insert elements" << endl;
+    cin >> num;
 
+    if (num == 0)
+      break;
 
-
-void forintegers()
-{
-  int num=0;
-  vector <int> vec;
-
-  while(true) { 
-	  cout << "Enter number ";
-      cin >> num;
-	  vec.push_back(num);
-      cout << "Do you want to enter another number y/n? ";
-	  char c;
-	  cin>>c;
-
-	  if(c == 'y')
-			continue;
-	  else
-            break;		  
-	  
+    vec.push_back(num);
   }
-  print_vector(vec);
-
-
-  vec.insert(vec.begin()+2,5,444);
-
-  print_vector(vec);
-
-vector <int> vec2(vec);
-
-
- print_vector(vec2);
- 
-//vec2.insert(vec.begin(),vec.begin(),vec.end());
-
-}
-void print_vector(vector <int> vec)
-{
-	 cout << "capacity "<< vec.capacity()  << "max size" << vec.max_size() << endl;
- 
-	for (int i=0; i<vec.size(); i++){
-		cout << "vec " << "=" <<vec.at(i) <<endl;
-
-	}
-	cout << "END" << endl;
 }
 
+void vector_practice::vec_list(void) {
+  cout << "******** List is ****** " << endl;
+  for (int i = 0; i < vec.size(); i++)
+    cout << vec.at(i) << endl;
+}
+void vector_practice::vec_insert(void) {
+  int pos, value;
+  cout << "Enter position to insert between 0 and " << vec.size() << endl;
+  cin >> pos;
+  cout << "Enter value";
+  cin >> value;
 
+  vec.insert(vec.begin() + pos, value);
+}
+int main() {
 
+  vector_practice vec_practice;
+  int choice;
+  while (1) {
+    cout << "choose option " << endl;
+    cout << "1 "
+         << "Add elements with push_back" << endl;
+    cout << "2 "
+         << "Add elements with insert" << endl;
+
+    cout << "3 "
+         << "List Elements" << endl;
+    cin >> choice;
+    switch (choice) {
+    case 1:
+      vec_practice.vec_add();
+      break;
+
+    case 2:
+
+      vec_practice.vec_insert();
+      break;
+
+    case 3:
+      vec_practice.vec_list();
+      break;
+    }
+  }
+  return 1;
+}
